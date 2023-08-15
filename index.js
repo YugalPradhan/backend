4a41ea17bd309e6db68750ac58fac7c7e2f24bd5
YugalPradhan <75770560+YugalPradhan@users.noreply.github.com>
2023-08-15 15:08:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const port=5000
 
 app.use(cors())
 app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
@@ -15,6 +20,3 @@ app.listen(port,()=>{
     console.log(`App listening at http://localhost:${port}`)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
